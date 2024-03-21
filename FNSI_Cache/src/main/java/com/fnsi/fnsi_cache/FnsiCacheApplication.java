b0d182c43d4aa32dd0f9fe54fab90c2d2979411b
Artem Martynov <amerta90@mail.ru>
2024-03-22 02:54:52 +0300

    Init commit

---
+++ FNSI_Cache/src/main/java/com/fnsi/fnsi_cache/FnsiCacheApplication.java
@@ -0,0 +1,13 @@
+package com.fnsi.fnsi_cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FnsiCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FnsiCacheApplication.class, args);
+    }
+
+}
