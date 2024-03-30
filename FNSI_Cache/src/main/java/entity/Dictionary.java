917b7f0eb8f3c6f1568d38aa4734c145c1f4fa80
Tiatiroo <vaoborozhniy@stud.etu.ru>
2024-03-30 20:35:37 +0300

    Issue-9. Создать сущность обертку для таблицы справочник.

---
+++ FNSI_Cache/src/main/java/entity/Dictionary.java
@@ -0,0 +1,22 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dictionaries")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Dictionary {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String system;
+    private String version;
+    private String code;
+    private String display;
+}
