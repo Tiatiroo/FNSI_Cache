01cfd61ea0c445f1b1ed05e44ca50953db6a8204
GoldSASdloG <Procatulus@gmail.com>
2024-04-03 10:54:01 +0300

    Serge01-PassportRepositoryReady

---
+++ FNSI_Cache/src/main/java/com/fnsi/fnsi_cache/dao/PassportRepository.java
@@ -0,0 +1,16 @@
+package com.fnsi.fnsi_cache.dao;
+
+import com.fnsi.fnsi_cache.entity.Dictionary;
+import com.fnsi.fnsi_cache.entity.Passport;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PassportRepository extends JpaRepository<Passport, Long> {
+   @Query(value = "select p from Passport p where p.system = :system and p.version = :version")
+   Optional<Passport> getPassport(@Param("system") String system, @Param("version") String version);
+}
