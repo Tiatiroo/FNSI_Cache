c3b930f89f77630b616987cfb8e0706e601267d1
tawad <tawadat@rambler.ru>
2024-04-03 16:35:35 +0400

    Issues 13-создать-репозиторий-для-работы-с-сущностью-маппинг-справочников исправлено

--- FNSI_Cache/src/main/java/com/fnsi/fnsi_cache/dao/MappingRepository.java
+++ FNSI_Cache/src/main/java/com/fnsi/fnsi_cache/dao/MappingRepository.java
@@ -1,8 +1,6 @@
 package com.fnsi.fnsi_cache.dao;
 
-import com.fnsi.fnsi_cache.entity.Dictionary;
 import com.fnsi.fnsi_cache.entity.Mapping;
-import com.fnsi.fnsi_cache.entity.Passport;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,9 +9,15 @@ import java.util.Optional;
 
 public interface MappingRepository extends JpaRepository<Mapping,Long> {
 
-        @Query(value = "select m from Mapping m where m.system =:system and (m.version = :version or m.version is null and " +
-                "NOT EXISTS(select m from Mapping m where m.system =:system and m.version =:version))")
+        @Query(value = "select m from Mapping m" +
+                " where m.system =:system " +
+                "and (m.version = :version " +
+                "or (m.version is null and " +
+                "NOT EXISTS(select m2 from Mapping m2 " +
+                "where m2.system =:system and m2.version =:version)))")
+
         Optional<Mapping> getMapping(@Param("system") String system, @Param("version") String version);
+
     }
 
 
