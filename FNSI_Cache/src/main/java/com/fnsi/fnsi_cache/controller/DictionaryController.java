db230d0e872ea47d7a8a0ae7cb63de02424aa72d
GoldSASdloG <Procatulus@gmail.com>
2024-04-10 11:38:37 +0300

    controller_dictionaryReady

--- FNSI_Cache/src/main/java/com/fnsi/fnsi_cache/controller/DictionaryController.java
+++ FNSI_Cache/src/main/java/com/fnsi/fnsi_cache/controller/DictionaryController.java
@@ -4,10 +4,8 @@ import com.fnsi.fnsi_cache.entity.Dictionary;
 import com.fnsi.fnsi_cache.entity.Mapping;
 import com.fnsi.fnsi_cache.service.DictionaryService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RequestMapping("/mappings")
 @RestController
 public class DictionaryController {
@@ -16,11 +14,29 @@ public class DictionaryController {
     public DictionaryController(DictionaryService dictionaryService) {
         this.dictionaryService = dictionaryService;
     }
+
     @GetMapping("/{system}/{version}/{code}")
     public Dictionary getDictionari(@PathVariable(name = "system")String system,
                                     @PathVariable(name = "version")String version,
                                     @PathVariable(name = "code")String code){
         Dictionary dictionary = dictionaryService.getDictionary(system, version, code);
         return dictionary;
+    }
+
+    @PutMapping("/")
+    public Dictionary replaceDict(@RequestBody Dictionary replaceDict){
+        return dictionaryService.updateDictionary(replaceDict);
+    }
+
+    @PostMapping("/")
+    public Dictionary newDictionary(@RequestBody Dictionary newDictionary){
+        return dictionaryService.addDictionary(newDictionary);
+    }
+
+    @DeleteMapping("/{system}/{version}/{code}")
+    public void deleteDict (@PathVariable(name = "system")String system,
+                            @PathVariable(name = "version")String version,
+                            @PathVariable(name = "code")String code){
+        dictionaryService.deleteDictionary(system, version, code);
     }
 }
